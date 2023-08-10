dd981a1893e6b71d181a4e445ab9903e4da521db
ChoSangok <chod1510@naver.com>
2023-08-10 14:10:20 +0900

    #4header(components,style)생성

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Head } from "@/styles/HeadStyle";
+
+const Header = () => {
+  return (
+    <>
+      <Head>asdas</Head>
+    </>
+  );
+};
+
+export default Header;
---
+++ client/src/styles/HeadStyle.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Head = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 56px;
+  border-top: 3px solid var(--orange);
+  border-bottom: 1px solid var(--black-200);
+`;
