49346637c3ccb9abf777a3f74b09eb221030cca8
ChoSangok <chod1510@naver.com>
2023-08-11 11:38:18 +0900

    #11 react-router-dom누락으로추가설치

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import GlobalStyle from "@/styles/global";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Sidebar from "@/components/Sidebar";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Header />
+      <Sidebar />
       <Footer />
     </>
   );
