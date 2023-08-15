fa1547b77b196d61ec59689bde54598d7315e578
ChoSangok <chod1510@naver.com>
2023-08-16 06:31:35 +0900

    #39 feat/contents게스트용화면기본레이아웃완성

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import Header from "@/components/Header";
-import { Outlet } from "react-router-dom";
+import Footer from "@/components/Footer";
+import { Contents } from "@/styles/Home/HomeStyle";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Contents>
+        <article>
+          <h1>FRONTEND</h1>
+          <div className="frontEnd"></div>
+        </article>
+        <article>
+          <h1>BACKEND</h1>
+          <div className="backEnd"></div>
+        </article>
+      </Contents>
+      <Footer />
     </>
   );
 };
--- client/src/styles/Home/HomeStyle.jsx
+++ client/src/styles/Home/HomeStyle.jsx
@@ -1,6 +1,30 @@
 import styled from "styled-components";
 
-export const Contents = styled.div`
-  width: 100vw;
-  height: 100vw;
+export const Contents = styled.main`
+  display: flex;
+  justify-content: center;
+
+  article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 200px 50px;
+  }
+
+  h1 {
+  }
+
+  .frontEnd {
+    width: 300px;
+    height: 300px;
+    background-color: var(--black);
+    border-radius: 30px;
+  }
+
+  .backEnd {
+    width: 300px;
+    height: 300px;
+    background-color: var(--black);
+    border-radius: 30px;
+  }
 `;
