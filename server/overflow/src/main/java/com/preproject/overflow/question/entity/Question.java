e97672ae2e169a1270c7f290056dd5de288d7175
ssklim <djshawnslim@gmail.com>
2023-08-18 04:43:14 +0900

    1차 commit

--- server/overflow/src/main/java/com/preproject/overflow/question/entity/Question.java
+++ server/overflow/src/main/java/com/preproject/overflow/question/entity/Question.java
@@ -2,16 +2,22 @@ package com.preproject.overflow.question.entity;
 
 import com.preproject.overflow.audit.Auditable;
 import com.preproject.overflow.member.entity.Member;
+import lombok.NoArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Arraylist;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+
 public class Question extends Auditable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,12 +27,43 @@ public class Question extends Auditable {
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @Column
+    @Column(nullable = false)
     private String title;
 
-    @Column
+    @Column(nullable = false)
     private String text;
 
-    @Column
-    private String tag;
+    private int voteCount;
+
+    private int answerCount;
+
+    private int viewCount;
+
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<QuestionTag> questionTagList = new ArrayList<>();
+
+    // 연관 관계 메서드
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public void addQuestionTag(QuestionTag questionTag) {
+        this.questionTagList.add(questionTag);
+        if(questionTag.getQuestion() != this){
+            questionTag.addQuestion(this);
+        }
+    }
+
+    public void addAnswer(Answer answer) {
+        this.answerList.add(answer);
+        if(answer.getQuestion() != this){
+            answer.setQuestion(this);
+        }
+    }
+
 }
